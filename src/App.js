fa262b2211396b488398e21e418a9a853c91cc7e
magdalena krasicka <krasickamagdalena@gmail.com>
2023-10-31 12:45:54 +0100

    form onchange

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -19,8 +21,11 @@ function Logo() {
   return <h1>🏝 Far Away 🧳</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
+    console.log(e);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
@@ -32,7 +37,15 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setDescription(e.target.value);
+        }}
+      />
       <button>Add</button>
     </form>
   );
