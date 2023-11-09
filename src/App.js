9c82a7787930fb3aca8c6291de9f71df6e0136f3
magdalena krasicka <krasickamagdalena@gmail.com>
2023-11-09 18:13:26 +0000

    added conditions

--- src/App.js
+++ src/App.js
@@ -119,6 +119,13 @@ function Item({ onDeleteItems, item, onTogglePackedItems }) {
 }
 
 function Stats({ items }) {
+  if (items.length === 0) {
+    return (
+      <footer className="stats">
+        <em> Start adding items to your packing list!👚👗👙</em>
+      </footer>
+    );
+  }
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
   const percentage = Math.round((numPacked / numItems) * 100);
@@ -126,8 +133,10 @@ function Stats({ items }) {
   return (
     <footer className="stats">
       <em>
-        🧳 You have {numItems} items in your list, and you already packed{" "}
-        {numPacked} ({percentage}%).
+        {percentage === 100
+          ? "You have everything ready to go ✈️"
+          : `🧳 You have ${numItems} items in your list, and you already packed ${" "}
+        ${numPacked} (${percentage}%)`}
       </em>
     </footer>
   );
