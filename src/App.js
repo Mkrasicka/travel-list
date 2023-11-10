6a0047951e0981d1115d834ed8bb211a929c3445
magdalena krasicka <krasickamagdalena@gmail.com>
2023-11-10 08:53:16 +0000

    sort items

--- src/App.js
+++ src/App.js
@@ -97,6 +97,10 @@ function PackingList({ items, onDeleteItems, onTogglePackedItems }) {
     sortedItems = items
       .slice()
       .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
 
   return (
     <div className="list">
